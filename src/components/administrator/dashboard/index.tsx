9fc5bfc176d6f86549caa9686d64c35df54fab9f
ApocalCrk <ferdyfirmansyah3026@gmail.com>
2024-06-23 21:29:13 +0700

    refactor: Remove TableOne component from dashboard page

--- src/components/administrator/dashboard/index.tsx
+++ src/components/administrator/dashboard/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import ChatCard from "../Chat/ChatCard";
-import TableOne from "../Tables/TableOne";
 import CardDataStats from "../CardDataStats";
 import useDashboard from '@/hooks/administrator/useDashboard';
 import { Dashboard } from '@/types/dashboard';
@@ -25,13 +24,6 @@ const DashboardPage: React.FC = () => {
         <CardDataStats title="Total Admin" total={data?.total_admin.toString() || '0'}>
           <FaUsers className="fill-primary dark:fill-white" />
         </CardDataStats>
-      </div>
-
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
-        </div>
-        <ChatCard />
       </div>
     </>
   );
